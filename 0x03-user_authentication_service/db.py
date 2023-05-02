d22f5a3ee2232921a896a1c822f4f93a1747060d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-05-02 11:36:25 +0100

    Implemented update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -65,7 +65,7 @@ class DB:
                     return user
         raise NoResultFound
 
-    def update_user(user_id: int, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
         Updates multiple user records with the arguments passed
         using they key and values
