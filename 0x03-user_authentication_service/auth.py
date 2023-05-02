2587480efe12ea0201aa2aa8bca2bd459655bd86
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-05-02 13:02:31 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,3 +25,30 @@ def _hash_password(password: str) -> bytes:
     """
     passwd = password.encode('utf-8')
     return bcrypt.hashpw(passwd, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registers a new user with the email and password passed
+        then returns the new User object.
+        Args:
+            email (str): email in string format
+            password (str): password in string format
+        Return:
+                if user does not exist previously, register and
+                return new User object else raise ValuError
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hash = _hash_password(password)
+            user = self._db.add_user(email, hash)
+            return user
+        raise ValueError(f"User {email} already exists")
